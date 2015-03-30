86ef524e896e56e0f8f337dbea6e8120925a7a1b
Julie Turner and Kathryn Carr <student@epicodus.com>
2015-03-30 13:07:05 -0700

    Create form and rambles posting section.

--- app/controllers/rambles_controller.rb
+++ app/controllers/rambles_controller.rb
@@ -1,6 +1,25 @@
 class RamblesController < ApplicationController
 
   def index
+    @rambles = Ramble.all
+  end
+
+  def create
+    @ramble = Ramble.new(ramble_params)
+    @ramble.time = Date.today
+    @ramble.user_id = current_user.id
+    if @ramble.save
+      flash[:notice] = "Ramble heard"
+    else
+      flash[:alert] = "There was a problem with your ramble"
+    end
+    redirect_to :back
+  end
+
+  private
+
+  def ramble_params
+    params.require(:ramble).permit(:content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "rambles#index"
   resources :users
   resources :rambles
 end
